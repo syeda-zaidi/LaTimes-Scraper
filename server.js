9f4ef8917ac1d7bc6884fc6a699036d1d715db29
syeda zaidi <zaidisyeda03@gmail.com>
2019-10-16 15:02:32 -0700

    nav bar

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.connect(MONGODB_URI,  { useNewUrlParser: true });
 
 
+app.get("/", function(err, res) {
+    res.render("index")
+});
 
 // Start the server
 app.listen(PORT, function() {
