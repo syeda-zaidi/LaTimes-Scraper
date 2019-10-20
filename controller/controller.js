ae63ab7963b89ae9c2166419b3d1f492f360abfa
syeda zaidi <zaidisyeda03@gmail.com>
2019-10-20 15:50:39 -0700

    controller setup

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,45 @@
-const axios = require ("axios");
+const axios = require("axios");
 const db = require("../models/index");
 const cheerio = require("cheerio");
 
 module.exports = app => {
-     
 
+    //return json for all saved articles 
+    app.get("/articles-json", function (req, res) {
+
+        db.Articles.find({})
+            .then(function (dbArticles) {
+                res.json(dbArticles);
+            }).catch(function (err) {
+                res.json(err);
+            });
+    });
+
+    app.get("/articles-json/:id", function (req, res) {
+        db.Articles.findOne({ _id: req.params.id })
+            .populate("Notes")
+            .then(function (dbArticles) {
+                res.json(dbArticles)
+            }).catch(function (err) {
+                res.json(err);
+            });
+    });
+
+    //updates the article for saving it
+    app.put("/saved/:id", function (req, res) {
+        ab.Articles.findOneAndUpdate({ _id: req.params.id }, { saved: true }, { new: true })
+            .then(function (savedArticle) {
+                res.sendStatus(200);
+                console.log("article saved : " + savedArticle);
+
+            }).catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            });
+    });
+
+
+    
 // scrapes new articles and saves in db
 app.get("/scrape", function (req, res) {
 
@@ -89,4 +124,8 @@ app.get("/scrape", function (req, res) {
     res.redirect("/articles-json");
   });
 
+
+
 }
+
+     
